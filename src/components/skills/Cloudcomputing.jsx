fb8e6699436f25dd48dfcbea2cb24aca101f40b3
sivaprakash <109328049+siva3010@users.noreply.github.com>
2025-03-13 13:05:40 +0530

    Update Cloudcomputing.jsx
    Updated the new details

--- src/components/skills/Cloudcomputing.jsx
+++ src/components/skills/Cloudcomputing.jsx
@@ -1,1 +1,60 @@
 
+import React from 'react'
+
+const BackEnd = () => {
+  return (
+    <div className="skills__content">
+        <h3 className="skills__title">Cloud Computing</h3>
+
+        <div className="skills__box">
+            <div className="skills__group">
+                <div className="skills__data">
+                    <i className="bx bx-badge-check"></i>
+                    <div>
+                        <h3 className="skills__name">Google Cloud</h3>
+                        <span className="skills__level">Intermediate</span>
+                    </div>
+                </div>
+
+                
+
+
+                <div className="skills__data">
+                    <i className="bx bx-badge-check"></i>
+                    <div>
+                        <h3 className="skills__name">Linux</h3>
+                        <span className="skills__level">Intermediate</span>
+                    </div>
+                </div>
+            </div>
+
+            
+
+            <div className="skills__group">
+
+            <div className="skills__data">
+                    <i className="bx bx-badge-check"></i>
+                    <div>
+                        <h3 className="skills__name">Awz</h3>
+                        <span className="skills__level">Advanced</span>
+                        
+                    </div>
+                </div>
+                <div className="skills__data">
+                    <i className="bx bx-badge-check"></i>
+                    <div>
+                        <h3 className="skills__name">VM, IAM ,VPN</h3>
+                        <span className="skills__level">Intermediate</span>
+                    </div>
+                </div>
+
+                
+
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default BackEnd
