d5d00c2aef3c28e67ee1cc2cb8f821905e52d2a8
sivaprakash <109328049+siva3010@users.noreply.github.com>
2025-06-26 10:52:37 +0000

    ....

---
+++ src/components/skills/Adobe.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+
+const Adobe = () => {
+  return (
+    <div className="skills__content">
+        <h3 className="skills__title">Adobe</h3>
+
+        <div className="skills__box">
+            <div className="skills__group">
+                <div className="skills__data">
+                    <i className="bx bx-badge-check"></i>
+                    <div>
+                        <h3 className="skills__name">Adobe Commerce</h3>
+                        <span className="skills__level">Advanced</span>
+                    </div>
+                </div>
+
+                <div className="skills__data">
+                    <i className="bx bx-badge-check"></i>
+                    <div>
+                    <h3 className="skills__name">AEP</h3>
+                        <span className="skills__level">Advanced</span>
+                        
+                    </div>
+                </div>
+
+                
+
+            </div>
+
+            <div className="skills__group">
+
+            <div className="skills__data">
+                    <i className="bx bx-badge-check"></i>
+                    <div>
+                        <h3 className="skills__name">AEM</h3>
+                        <span className="skills__level">Intermediate</span>
+                    </div>
+                </div>
+                
+                
+
+                <div className="skills__data">
+                    <i className="bx bx-badge-check"></i>
+                    <div>
+                        <h3 className="skills__name">AJO/RT-CDP</h3>
+                        <span className="skills__level">Intermediate</span>
+                    </div>
+                </div>
+
+               
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Adobe
